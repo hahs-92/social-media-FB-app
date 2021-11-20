70a0707363ca4720bc24a9166046fa285e190bae
Hugo Alexander Hernandez Serrano <alexandre_2420@outlook.com>
2021-11-20 09:56:31 -0500

    setup initial

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+//styles
+import styles from '../styles/pages/Home.styles.modules.css'
+
+const Home = () => {
+    return (
+        <div className={styles.Home }>
+            Home
+        </div>
+    )
+}
+
+export default Home
