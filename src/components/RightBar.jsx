265e6a5e52584babd1b8900ea45882066c734641
Hugo Alexander Hernandez Serrano <alexandre_2420@outlook.com>
2021-11-22 11:40:07 -0500

    RightBar componente

--- src/components/RightBar.jsx
+++ src/components/RightBar.jsx
@@ -2,9 +2,79 @@ import styles from '../styles/components/RightBar.styles.module.css'
 
 const RightBar = () => {
     return (
-        <article className={ styles.RightBar }>
-            RightBar
-        </article>
+        <section className={ styles.RightBar }>
+            <div className={ styles.Wrapper }>
+                <article className={ styles.BirthDayWrapper}>
+                    <img className={ styles.BirthDayImage } src="/assets/gift.png" alt="gift" />
+                    <span className={ styles.BirthDayText }>
+                        <b>Jess Gray</b> and <b>3 other friends</b> have a birthday today
+                    </span>
+                </article>
+
+                <img className={ styles.Ad } src="assets/ad.png" alt="ad" />
+
+                <h4 className={ styles.Title }>
+                    Online Friends
+                </h4>
+
+                <ul className={ styles.FriendList}>
+                    <li className={ styles.Friend }>
+                        <article className={ styles.ProfileImageWrapper }>
+                            <img className={ styles.ProfileImage } src="assets/person/3.jpeg" alt="person-profile" />
+
+                            <span className={ styles.Online}></span>
+                        </article>
+
+                        <span className={ styles.UserName }>Jhon Carter</span>
+                    </li>
+                    <li className={ styles.Friend }>
+                        <article className={ styles.ProfileImageWrapper }>
+                            <img className={ styles.ProfileImage } src="assets/person/3.jpeg" alt="person-profile" />
+
+                            <span className={ styles.Online}></span>
+                        </article>
+
+                        <span className={ styles.UserName }>Jhon Carter</span>
+                    </li>
+                    <li className={ styles.Friend }>
+                        <article className={ styles.ProfileImageWrapper }>
+                            <img className={ styles.ProfileImage } src="assets/person/3.jpeg" alt="person-profile" />
+
+                            <span className={ styles.Online}></span>
+                        </article>
+
+                        <span className={ styles.UserName }>Jhon Carter</span>
+                    </li>
+                    <li className={ styles.Friend }>
+                        <article className={ styles.ProfileImageWrapper }>
+                            <img className={ styles.ProfileImage } src="assets/person/3.jpeg" alt="person-profile" />
+
+                            <span className={ styles.Online}></span>
+                        </article>
+
+                        <span className={ styles.UserName }>Jhon Carter</span>
+                    </li>
+                    <li className={ styles.Friend }>
+                        <article className={ styles.ProfileImageWrapper }>
+                            <img className={ styles.ProfileImage } src="assets/person/3.jpeg" alt="person-profile" />
+
+                            <span className={ styles.Online}></span>
+                        </article>
+
+                        <span className={ styles.UserName }>Jhon Carter</span>
+                    </li>
+                    <li className={ styles.Friend }>
+                        <article className={ styles.ProfileImageWrapper }>
+                            <img className={ styles.ProfileImage } src="assets/person/3.jpeg" alt="person-profile" />
+
+                            <span className={ styles.Online}></span>
+                        </article>
+
+                        <span className={ styles.UserName }>Jhon Carter</span>
+                    </li>
+                </ul>
+            </div>
+        </section>
     )
 }
 
